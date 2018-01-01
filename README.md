72378fe13ccf1e3aefbd7512dd8baaec649e0d3f
cuuprum <tembagajingga@gmail.com>
2018-01-01 21:29:03 +0700

    Finish install Pug and load index.pug

--- README.md
+++ README.md
@@ -2,11 +2,16 @@
 ## This project will be Knowledgebase website base on nodeJS using Express Framework and PUG tempalte for front end
 
 ## TODO
- * PART 1
- * [x] Setup environtment
+ PART 1
+   * [x] Setup environtment
 
- * PART 2
- * [x] Create project, init using npm init and write package.json to describe dependencies
- * [x]  Create app.js as starting file and set npm script "start" to start app.js as first file starting Project
- * [x] Install Express Framework using npm and --save to register it to dependencies
- * [x] Create simple server, route and display hello wolrd  
+ PART 2
+   * [x] Create project, init using npm init and write package.json to describe dependencies
+   * [x]  Create app.js as starting file and set npm script "start" to start app.js as first file starting Project
+   * [x] Install Express Framework using npm and --save to register it to dependencies
+   * [x] Create simple server, route and display hello wolrd  
+
+Part 3
+  * [x] Install Pug template engine, setting app.js to run the view (index.pug)
+  * [ ] Basic Pug html
+  * [ ]
--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express'); // Import library express
+const path = require('path'); // Import lib path dr core lib nodejs
 
+// Init App
 const app = express(); // Bikin variable yang bs jalanin fungsi express
 
+// Load View Engine
+// Object views direct ke path yang ada view nya
+app.set('views', path.join(__dirname, 'views')); // __dirname = lokasi folder ini skrg, "views" nama folder di tuju, jadi nodekb/views
+app.set('view engine', 'pug');
+
+// Home Route
 // Buat route, yang kalau kita akses (request halaman) / (root) dia akan handle request kita
 app.get('/', function(req, res){ // request ke app directory / (root), kalau udah ke akses jalanin callback function(req,res) jadi kita bs punya akses untuk jalnin object request dan response
-  res.send('Hello Wolrd~'); // Berikan respon ke browser, isinya send "hello world"
+  //res.send('Hello Wolrd~'); // Berikan respon ke browser, isinya send "hello world"
+  res.render('index');
 });
 
+// Start Server
 app.listen(3000, ()=>{ // Port mana yang bs di akses dari browser? server hanya akan listen ke port mana gitu
   console.log('Server started on port 3000. . .'); // Setelah listen, kasih callback (fungsi yg dijalanin setelah listen), tampilkan pesan sedang listening
 });
