4fc33102748d9edcf36473be8830353ff251b7ba
cuuprum <tembagajingga@gmail.com>
2018-01-01 19:37:33 +0700

    Finish create simple server

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 ## This project will be Knowledgebase website base on nodeJS using Express Framework and PUG tempalte for front end
 
 ## TODO
+ * PART 1
+ * [x] Setup environtment
 
+ * PART 2
  * [x] Create project, init using npm init and write package.json to describe dependencies
  * [x]  Create app.js as starting file and set npm script "start" to start app.js as first file starting Project
  * [x] Install Express Framework using npm and --save to register it to dependencies
- * [ ] Create simple server, route and display hello wolrd  
+ * [x] Create simple server, route and display hello wolrd  
--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Hello world');
+const express = require('express'); // Import library express
+
+const app = express(); // Bikin variable yang bs jalanin fungsi express
+
+// Buat route, yang kalau kita akses (request halaman) / (root) dia akan handle request kita
+app.get('/', function(req, res){ // request ke app directory / (root), kalau udah ke akses jalanin callback function(req,res) jadi kita bs punya akses untuk jalnin object request dan response
+  res.send('Hello Wolrd~'); // Berikan respon ke browser, isinya send "hello world"
+});
+
+app.listen(3000, ()=>{ // Port mana yang bs di akses dari browser? server hanya akan listen ke port mana gitu
+  console.log('Server started on port 3000. . .'); // Setelah listen, kasih callback (fungsi yg dijalanin setelah listen), tampilkan pesan sedang listening
+});
